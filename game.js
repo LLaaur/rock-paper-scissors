eecae2d14b2d29300692bcd93e8c5a2c375135b4
Laur <laur_laurentiu2013@yahoo.com>
2022-06-21 17:38:26 +0300

    add animation to both hands

--- game.js
+++ game.js
@@ -2,11 +2,13 @@ const introScreen = document.querySelector('.intro-screen');
 const startBtn = document.querySelector('.start-game');
 const loadScreen = document.querySelector('.wrapper');
 const gameScreen = document.querySelector('.game-screen');
+const playerHand = document.querySelector('.fist-left');
+const computerHand = document.querySelector('.fist-right');
 
 function showGame() {
     loadScreen.style.display = 'none';
     gameScreen.style.display = 'flex';
-}
+};
 
 swapScreens = function () {
     if (startBtn) {
@@ -14,4 +16,9 @@ swapScreens = function () {
         loadScreen.style.display = 'flex';
         setTimeout(showGame, 2000);
     }
-}
+};
+
+triggerAnimation = function(){
+    playerHand.style.animation = 'fistLeft 2s';
+    computerHand.style.animation = 'fistRight 2s';
+};
